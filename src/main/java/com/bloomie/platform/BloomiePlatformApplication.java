a2e1cbf30aa7a4470bfc48ef74dff0358b093c20
luuu6 <lucianamechan@gmail.com>
2026-06-04 20:48:12 -0500

    chore: initial project structure

---
+++ src/main/java/com/bloomie/platform/BloomiePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.bloomie.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomiePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomiePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bloomie/platform/BloomiePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloomie.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomiePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
