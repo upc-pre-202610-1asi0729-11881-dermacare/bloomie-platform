55fae47dab3beec5fa2637c88898021294e39e48
luuu6 <lucianamechan@gmail.com>
2026-06-12 19:36:30 -0500

    feat(select-subscription-plan): add resources for interfaces layer

---
+++ src/main/java/com/bloomie/platform/subscription/infrastructure/repositories/SubscriptionPersistenceRepository.java
@@ -0,0 +1,22 @@
+// SubscriptionPersistenceRepository.java
+package com.bloomie.platform.subscription.infrastructure.repositories;
+
+import com.bloomie.platform.subscription.domain.model.valueobjects.PatientId;
+import com.bloomie.platform.subscription.infrastructure.entities.SubscriptionPersistenceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SubscriptionPersistenceRepository
+        extends JpaRepository<SubscriptionPersistenceEntity, Long> {
+
+    @Query("select s from SubscriptionPersistenceEntity s where s.patientId = :patientId")
+    Optional<SubscriptionPersistenceEntity> findByPatientId(@Param("patientId") PatientId patientId);
+
+    @Query("select count(s) from SubscriptionPersistenceEntity s where s.patientId = :patientId")
+    long countByPatientId(@Param("patientId") PatientId patientId);
+}
