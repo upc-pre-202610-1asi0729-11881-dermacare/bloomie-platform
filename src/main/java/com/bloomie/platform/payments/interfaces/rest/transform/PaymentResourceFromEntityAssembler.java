af3f26d169bb3b9db54743d7197b0f937816d94c
luuu6 <lucianamechan@gmail.com>
2026-06-14 13:24:59 -0500

    add ProcessSubscriptionPaymentResource

---
+++ src/main/java/com/bloomie/platform/payments/interfaces/rest/transform/PaymentResourceFromEntityAssembler.java
@@ -0,0 +1,8 @@
+package com.bloomie.platform.payments.interfaces.rest.transform;
+
+import com.bloomie.platform.payments.domain.model.aggregates.Payment;
+import com.bloomie.platform.payments.interfaces.rest.resources.PaymentResource;
+
+public class PaymentResourceFromEntityAssembler {
+
+}
