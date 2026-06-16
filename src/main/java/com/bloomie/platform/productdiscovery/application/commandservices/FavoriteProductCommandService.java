1e44fe950d7c47f0aeea6aa3eab525d7cfb67e91
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-15 21:56:01 -0500

    feat(product-discovery): add favorite product command service.

---
+++ src/main/java/com/bloomie/platform/productdiscovery/application/commandservices/FavoriteProductCommandService.java
@@ -0,0 +1,20 @@
+package com.bloomie.platform.productdiscovery.application.commandservices;
+
+import com.bloomie.platform.productdiscovery.domain.model.aggregates.FavoriteProduct;
+import com.bloomie.platform.productdiscovery.domain.model.commands.SaveProductAsFavoriteCommand;
+import com.bloomie.platform.shared.application.result.ApplicationError;
+import com.bloomie.platform.shared.application.result.Result;
+
+/**
+ * Application service contract for commands over favorite products.
+ */
+public interface FavoriteProductCommandService {
+
+    /**
+     * Handles saving a product as a favorite for a user.
+     *
+     * @param command command containing productId and userId
+     * @return the saved favorite product or an application error
+     */
+    Result<FavoriteProduct, ApplicationError> handle(SaveProductAsFavoriteCommand command);
+}
