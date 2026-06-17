089996763c86a7d06e76749e901841dee4b2f3c9
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-17 16:54:02 -0500

    feat(routine-management): add Routine Item Status value object.

---
+++ src/main/java/com/bloomie/platform/routinemanagement/domain/model/valueobjects/RoutineItemStatus.java
@@ -0,0 +1,8 @@
+package com.bloomie.platform.routinemanagement.domain.model.valueobjects;
+
+/**
+ * Lifecycle status of a single routine item.
+ */
+public enum RoutineItemStatus {
+    PENDING, APPLIED
+}
