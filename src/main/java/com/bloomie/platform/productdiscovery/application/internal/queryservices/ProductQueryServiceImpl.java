7971e65705936cfe0f0b2a5e598388a3ce537029
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-16 20:13:43 -0500

    feat(product-discovery): update product query service implementation.

--- src/main/java/com/bloomie/platform/productdiscovery/application/internal/queryservices/ProductQueryServiceImpl.java
+++ src/main/java/com/bloomie/platform/productdiscovery/application/internal/queryservices/ProductQueryServiceImpl.java
@@ -3,10 +3,12 @@ package com.bloomie.platform.productdiscovery.application.internal.queryservices
 import com.bloomie.platform.productdiscovery.application.queryservices.ProductQueryService;
 import com.bloomie.platform.productdiscovery.domain.model.aggregates.Product;
 import com.bloomie.platform.productdiscovery.domain.model.queries.GetAllProductsQuery;
+import com.bloomie.platform.productdiscovery.domain.model.queries.GetProductByIdQuery;
 import com.bloomie.platform.productdiscovery.domain.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Application service that resolves product read queries.
@@ -23,5 +25,10 @@ public class ProductQueryServiceImpl implements ProductQueryService {
     @Override
     public List<Product> handle(GetAllProductsQuery query) {
         return productRepository.findAll();
+    }
+
+    @Override
+    public Optional<Product> handle(GetProductByIdQuery query) {
+        return productRepository.findById(query.productId());
     }
 }
