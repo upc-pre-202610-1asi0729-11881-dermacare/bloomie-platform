99704020c419d89db868fb7a306677abcd4171f0
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-17 20:33:51 -0500

    feat(routine-management): update routine controller.

--- src/main/java/com/bloomie/platform/routinemanagement/interfaces/rest/RoutineController.java
+++ src/main/java/com/bloomie/platform/routinemanagement/interfaces/rest/RoutineController.java
@@ -1,12 +1,17 @@
 package com.bloomie.platform.routinemanagement.interfaces.rest;
 
+import com.bloomie.platform.routinemanagement.application.commandservices.RoutineCommandService;
 import com.bloomie.platform.routinemanagement.application.queryservices.RoutineQueryService;
 import com.bloomie.platform.routinemanagement.domain.model.queries.GetAllRoutinesQuery;
 import com.bloomie.platform.routinemanagement.domain.model.queries.GetRoutineByIdQuery;
+import com.bloomie.platform.routinemanagement.interfaces.rest.resources.CreateRoutineResource;
 import com.bloomie.platform.routinemanagement.interfaces.rest.resources.RoutineResource;
+import com.bloomie.platform.routinemanagement.interfaces.rest.transform.CreateRoutineCommandFromResourceAssembler;
 import com.bloomie.platform.routinemanagement.interfaces.rest.transform.RoutineResourceFromEntityAssembler;
 import com.bloomie.platform.shared.application.result.ApplicationError;
+import com.bloomie.platform.shared.application.result.Result;
 import com.bloomie.platform.shared.interfaces.rest.transform.ErrorResponseAssembler;
+import com.bloomie.platform.shared.interfaces.rest.transform.ResponseEntityAssembler;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
@@ -15,6 +20,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,10 +37,45 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Tag(name = "Routines", description = "Routine management routine endpoints")
 public class RoutineController {
 
+    private final RoutineCommandService routineCommandService;
     private final RoutineQueryService routineQueryService;
 
-    public RoutineController(RoutineQueryService routineQueryService) {
+    public RoutineController(RoutineCommandService routineCommandService, RoutineQueryService routineQueryService) {
+        this.routineCommandService = routineCommandService;
         this.routineQueryService = routineQueryService;
+    }
+
+    /**
+     * Create a new personalized skincare routine.
+     *
+     * @param resource the resource containing routine creation data
+     * @return the created routine resource
+     */
+    @PostMapping
+    @Operation(
+            summary = "Create a routine",
+            description = "Creates a new personalized skincare routine for a user."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Routine created successfully",
+                    content = @Content(schema = @Schema(implementation = RoutineResource.class))
+            ),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "500", description = "Unexpected error")
+    })
+    public ResponseEntity<?> createRoutine(@RequestBody CreateRoutineResource resource) {
+        var command = CreateRoutineCommandFromResourceAssembler.toCommandFromResource(resource);
+        var result = routineCommandService.handle(command)
+                .flatMap(routineId -> routineQueryService.handle(new GetRoutineByIdQuery(routineId))
+                        .<Result<com.bloomie.platform.routinemanagement.domain.model.aggregates.Routine, ApplicationError>>map(Result::success)
+                        .orElseGet(() -> Result.failure(ApplicationError.notFound("Routine", routineId.toString()))));
+        return ResponseEntityAssembler.toResponseEntityFromResult(
+                result,
+                RoutineResourceFromEntityAssembler::toResourceFromEntity,
+                HttpStatus.CREATED
+        );
     }
 
     /**
