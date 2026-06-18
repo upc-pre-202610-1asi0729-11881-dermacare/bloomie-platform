e8dbc3f92c64dd86e94a398bba6ddd1def794ced
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-17 20:30:34 -0500

    feat(routine-management): add routine command service implementation.

---
+++ src/main/java/com/bloomie/platform/routinemanagement/application/internal/commandservices/RoutineCommandServiceImpl.java
@@ -0,0 +1,33 @@
+package com.bloomie.platform.routinemanagement.application.internal.commandservices;
+
+import com.bloomie.platform.routinemanagement.application.commandservices.RoutineCommandService;
+import com.bloomie.platform.routinemanagement.domain.model.aggregates.Routine;
+import com.bloomie.platform.routinemanagement.domain.model.commands.CreateRoutineCommand;
+import com.bloomie.platform.routinemanagement.domain.repositories.RoutineRepository;
+import com.bloomie.platform.shared.application.result.ApplicationError;
+import com.bloomie.platform.shared.application.result.Result;
+import org.springframework.stereotype.Service;
+
+/**
+ * Application service that executes routine commands.
+ */
+@Service
+public class RoutineCommandServiceImpl implements RoutineCommandService {
+
+    private final RoutineRepository routineRepository;
+
+    public RoutineCommandServiceImpl(RoutineRepository routineRepository) {
+        this.routineRepository = routineRepository;
+    }
+
+    @Override
+    public Result<Long, ApplicationError> handle(CreateRoutineCommand command) {
+        var routine = new Routine(command);
+        try {
+            routine = routineRepository.save(routine);
+        } catch (Exception e) {
+            return Result.failure(ApplicationError.unexpected("create-routine", e.getMessage()));
+        }
+        return Result.success(routine.getId());
+    }
+}
