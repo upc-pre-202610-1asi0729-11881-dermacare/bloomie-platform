1c2968d6c5bbd968280c73515da9c5190eb54037
luuu6 <lucianamechan@gmail.com>
2026-06-18 23:22:55 -0500

    feat(routine-management): add handle GetRecommendedProductsForRoutineItemQuery to RoutineQueryService interface

--- src/main/java/com/bloomie/platform/routinemanagement/application/queryservices/RoutineQueryService.java
+++ src/main/java/com/bloomie/platform/routinemanagement/application/queryservices/RoutineQueryService.java
@@ -1,9 +1,12 @@
 package com.bloomie.platform.routinemanagement.application.queryservices;
 
 import com.bloomie.platform.routinemanagement.domain.model.aggregates.Routine;
+import com.bloomie.platform.routinemanagement.domain.model.queries.GetOptionsForSkinTypeAndStep;
+import com.bloomie.platform.routinemanagement.domain.model.queries.GetRecommendedProductsForRoutineItemQuery;
 import com.bloomie.platform.routinemanagement.domain.model.queries.GetRoutineByIdQuery;
 import com.bloomie.platform.routinemanagement.domain.model.queries.GetRoutineByPatientIdQuery;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -26,4 +29,20 @@ public interface RoutineQueryService {
      * @return matching routine, if found
      */
     Optional<Routine> handle(GetRoutineByPatientIdQuery query);
+
+    /**
+     * Returns the 4 product replacement options for the given routine item.
+     *
+     * @param query routine and item identifiers
+     * @return ordered list of product name alternatives
+     */
+    List<String> handle(GetRecommendedProductsForRoutineItemQuery query);
+
+    /**
+     * Returns the 4 product options for the given skin type and routine step.
+     *
+     * @param query skin type and step
+     * @return ordered list of product name alternatives
+     */
+    List<String> handle(GetOptionsForSkinTypeAndStep query);
 }
