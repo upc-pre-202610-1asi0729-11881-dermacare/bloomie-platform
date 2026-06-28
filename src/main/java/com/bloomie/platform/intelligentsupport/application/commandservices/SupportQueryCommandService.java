eea8bcecb434406ab6a26ff2c79b31c2b519ae2b
luuu6 <lucianamechan@gmail.com>
2026-06-28 02:04:24 -0500

    add SupportQueryCommandService

---
+++ src/main/java/com/bloomie/platform/intelligentsupport/application/commandservices/SupportQueryCommandService.java
@@ -0,0 +1,10 @@
+package com.bloomie.platform.intelligentsupport.application.commandservices;
+
+import com.bloomie.platform.intelligentsupport.domain.model.aggregates.SupportQuery;
+import com.bloomie.platform.intelligentsupport.domain.model.commands.CreateSupportQueryCommand;
+import com.bloomie.platform.shared.application.result.ApplicationError;
+import com.bloomie.platform.shared.application.result.Result;
+
+public interface SupportQueryCommandService {
+    Result<SupportQuery, ApplicationError> handle(CreateSupportQueryCommand command);
+}
