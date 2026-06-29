e344964705e7bb50a75e0139ae2cc7e7390d8003
luuu6 <lucianamechan@gmail.com>
2026-06-29 02:00:04 -0500

    refactor(subscription): update SubscriptionCommandService

--- src/main/java/com/bloomie/platform/subscription/application/commandservices/SubscriptionCommandService.java
+++ src/main/java/com/bloomie/platform/subscription/application/commandservices/SubscriptionCommandService.java
@@ -4,6 +4,7 @@ import com.bloomie.platform.shared.application.result.ApplicationError;
 import com.bloomie.platform.shared.application.result.Result;
 import com.bloomie.platform.subscription.domain.model.aggregates.Subscription;
 import com.bloomie.platform.subscription.domain.model.commands.CancelSubscriptionCommand;
+import com.bloomie.platform.subscription.domain.model.commands.RenewSubscriptionCommand;
 import com.bloomie.platform.subscription.domain.model.commands.SelectSubscriptionPlanCommand;
 
 public interface SubscriptionCommandService {
@@ -11,4 +12,6 @@ public interface SubscriptionCommandService {
     Result<Long, ApplicationError> handle(SelectSubscriptionPlanCommand command);
 
     Result<Subscription, ApplicationError> handle(CancelSubscriptionCommand command);
+
+    Result<Subscription, ApplicationError> handle(RenewSubscriptionCommand command);
 }
