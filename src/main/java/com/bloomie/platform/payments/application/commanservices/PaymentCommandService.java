39920ead667209540d1ca21762ec19416b4a6ba3
luuu6 <lucianamechan@gmail.com>
2026-06-29 02:52:46 -0500

    refactor(payments): update PaymentCommandService

--- src/main/java/com/bloomie/platform/payments/application/commanservices/PaymentCommandService.java
+++ src/main/java/com/bloomie/platform/payments/application/commanservices/PaymentCommandService.java
@@ -1,6 +1,7 @@
 package com.bloomie.platform.payments.application.commanservices;
 
 import com.bloomie.platform.payments.domain.model.aggregates.Payment;
+import com.bloomie.platform.payments.domain.model.commands.ProcessRenewalPaymentCommand;
 import com.bloomie.platform.payments.domain.model.commands.ProcessSubscriptionPaymentCommand;
 import com.bloomie.platform.shared.application.result.ApplicationError;
 import com.bloomie.platform.shared.application.result.Result;
@@ -14,7 +15,16 @@ public interface PaymentCommandService {
      *
      * @param command The {@link ProcessSubscriptionPaymentCommand} command
      * @return A {@link Result} containing the created {@link Payment} on success,
-     *         or an {@link ApplicationError} on failure (validation or business rule violation)
+     *         or an {@link ApplicationError} on failure
      */
     Result<Payment, ApplicationError> handle(ProcessSubscriptionPaymentCommand command);
+
+    /**
+     * Handle Process Renewal Payment Command
+     *
+     * @param command The {@link ProcessRenewalPaymentCommand} command
+     * @return A {@link Result} containing the created renewal {@link Payment} on success,
+     *         or an {@link ApplicationError} on failure
+     */
+    Result<Payment, ApplicationError> handle(ProcessRenewalPaymentCommand command);
 }
