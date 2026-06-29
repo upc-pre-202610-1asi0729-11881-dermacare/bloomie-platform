4b04ec03518b22a19a7ff062eb37f55914d464f7
luuu6 <lucianamechan@gmail.com>
2026-06-29 02:52:47 -0500

    refactor(subscription): update SubscriptionCommandService

--- src/main/java/com/bloomie/platform/subscription/application/commandservices/SubscriptionCommandService.java
+++ src/main/java/com/bloomie/platform/subscription/application/commandservices/SubscriptionCommandService.java
@@ -3,6 +3,7 @@ package com.bloomie.platform.subscription.application.commandservices;
 import com.bloomie.platform.shared.application.result.ApplicationError;
 import com.bloomie.platform.shared.application.result.Result;
 import com.bloomie.platform.subscription.domain.model.aggregates.Subscription;
+import com.bloomie.platform.subscription.domain.model.commands.ActivateSubscriptionCommand;
 import com.bloomie.platform.subscription.domain.model.commands.CancelSubscriptionCommand;
 import com.bloomie.platform.subscription.domain.model.commands.ExpireSubscriptionCommand;
 import com.bloomie.platform.subscription.domain.model.commands.RenewSubscriptionCommand;
@@ -11,6 +12,8 @@ import com.bloomie.platform.subscription.domain.model.commands.SelectSubscriptio
 public interface SubscriptionCommandService {
 
     Result<Long, ApplicationError> handle(SelectSubscriptionPlanCommand command);
+
+    Result<Subscription, ApplicationError> handle(ActivateSubscriptionCommand command);
 
     Result<Subscription, ApplicationError> handle(CancelSubscriptionCommand command);
 
