30173ddbfd6f60a325f6d4a8914e7a91e6d71b1c
Arturouu <arturocontrerastorres86@gmail.com>
2026-07-04 01:39:09 -0500

    refactor(dermatological-appointment): remove paymentId from appointment resource

--- src/main/java/com/bloomie/platform/dermatologicalappointment/interfaces/rest/resources/AppointmentResource.java
+++ src/main/java/com/bloomie/platform/dermatologicalappointment/interfaces/rest/resources/AppointmentResource.java
@@ -7,7 +7,6 @@ public record AppointmentResource(
         @Schema(example = "1") Long id,
         @Schema(example = "1") Long patientId,
         @Schema(example = "2") Long dermatologistId,
-        @Schema(example = "10") Long paymentId,
         @Schema(example = "2025-12-25T10:00:00") String scheduledAt,
         @Schema(example = "SCHEDULED") String status,
         @Schema(example = "Personal emergency") String cancellationReason) {
